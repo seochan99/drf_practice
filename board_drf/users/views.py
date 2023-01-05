8af30bd52ef4b8e1eae6fc328f322b496ee51459
HuiChan Seo <78739194+seochan99@users.noreply.github.com>
2023-01-05 15:38:04 +0900

    feat : profileVeiw

--- board_drf/users/views.py
+++ board_drf/users/views.py
@@ -2,7 +2,8 @@ from django.contrib.auth.models import User
 from rest_framework import generics, status
 from rest_framework.response import Response
 
-from .serializers import RegisterSerializer, LoginSerializer
+from .serializers import RegisterSerializer, LoginSerializer, ProfileSerializer
+from .models import Profile
 
 # createAPIVIEW 사용 구현 
 class RegisterView(generics.CreateAPIView): 
@@ -18,3 +19,10 @@ class LoginView(generics.GenericAPIView):
         serializer.is_valid(raise_exception = True)                     # 검증 
         token = serializer.validated_data                               # validate()의 return값인 Token을 받아온다.
         return Response({"toekn":token.key}, status=status.HTTP_200_OK) # 반환
+
+# 가져오는 기능, 수정 기능 필요
+# 보는건 누구나, 수정은 해당 프로필의 주인만 
+# permission_classes field 설정
+class ProfileView(generics.RetrieveAPIView):
+    queryset = Profile.objects.all()
+    serializer_class = ProfileSerializer
