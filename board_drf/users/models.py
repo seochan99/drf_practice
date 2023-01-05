574b2c67bf561dd20611fc9a3a7c16200c111376
HuiChan Seo <78739194+seochan99@users.noreply.github.com>
2023-01-05 15:25:24 +0900

    feat : model 확장 - 1:1

--- board_drf/users/models.py
+++ board_drf/users/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
-# Create your models here.
+# Profile model : 유저모델 확장을 위해
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)   #primary_key = pk로 설정하여 통합적 관리
+    nickname = models.CharField(max_length=128)
+    position = models.CharField(max_length=128)
+    subjects = models.CharField(max_length=128)
+    image = models.ImageField(upload_to='profile/',deafult='deafult.png')
+
+# post_saved이벤트 발생시 프로필 생성 
+# 직접 프로필 생성 코드 작성 하지 않아도 유저 생성 이벤트 감지해 프로필 자동생성 가능
+@receiver(post_save, sender = User)
+def create_user_profile(sender,instance,created,**kwargs):
+    if created:
+        Profile.objects.create(user=instance)
--- board_drf/users/urls.py
+++ board_drf/users/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import RegisterView
+from .views import *
 
 # as_view()
 urlpatterns = [
     path('register/', RegisterView.as_view()),
+    path('login/',LoginView.as_view()),
 ]
