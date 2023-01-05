406dc532093c186ad28973e86987279228c1b7e4
HuiChan Seo <78739194+seochan99@users.noreply.github.com>
2023-01-05 14:36:22 +0900

    feat : user views.py

--- board_drf/users/views.py
+++ board_drf/users/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.contrib.auth.models import User
+from rest_framework import generics
 
-# Create your views here.
+from .serializers import RegisterSerializer
+
+# createAPIVIEW 사용 구현 
+class RegisterView(generics.CreateAPIView): 
+    queryset = User.objects.all()
+    serializer_class = RegisterSerializer
